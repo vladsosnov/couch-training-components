7c1e45dcf6d9709a660f924a2a2fe1b88df1005c
Vlad Sosnov <vladsosnov2001@gmail.com>
2021-02-10 21:38:57 +0200

    set dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const ImageminPlugin = require("imagemin-webpack")
@@ -23,7 +22,6 @@ const optimization = () => {
 
   if (isProd) {
     config.minimizer = [
-      new UglifyJsPlugin(),
       new OptimizeCssAssetWebpackPlugin(),
       new TerserWebpackPlugin()
     ]
