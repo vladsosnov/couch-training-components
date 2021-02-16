6ad81265b1bd5ed8ee1f23803701193a6afe0c63
Vlad Sosnov <vladsosnov2001@gmail.com>
2021-02-16 21:40:12 +0200

    add adaptive to components

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,21 @@ import $ from 'jquery'
 import '../index.html'
 import 'bootstrap';
 import '../css/main.css'
+
+import noUiSlider from 'nouislider';
+import 'nouislider/distribute/nouislider.css';
+
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    },
+    tooltips: true,
+    direction: 'rtl',
+    orientation: 'vertical',
+});
+
